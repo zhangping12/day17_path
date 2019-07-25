07f17c79b3df7e044a1672be0dbe8e5b1c74a924
ZP <634001184@qq.com>
2019-07-25 09:38:13 +0800

    update

--- src/src/gz/itcast/util/TestPath.java
+++ src/src/gz/itcast/util/TestPath.java
@@ -1,4 +1,23 @@
 package src.gz.itcast.util;
 
-public class TestPath {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+
+public class TestPath extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //连接数据库
+        //获取连接
+        Connection conn = JdbcUtil.getConnection();
+        System.out.println(conn);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
